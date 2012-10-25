79b0a14232e29db2160d88f9443c1a5fd36ed5f1
xiangyuan <liyajie1209@gmail.com>
2012-10-25 23:45:31 +0800

    insert an url function in views

--- IWuo/IWuo/urls.py
+++ IWuo/IWuo/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import patterns, include, url
 from IWuo.views import hello
+from IWuo.views import mytime
+
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -16,4 +18,5 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
     ('^hello/$',hello),
+    ('^mytime/$',mytime),
 )
--- IWuo/IWuo/views.py
+++ IWuo/IWuo/views.py
@@ -1,5 +1,11 @@
 #my first djang view
 from django.http import HttpResponse
+from datetime import datetime
 
 def hello(request):
-    return HttpResponse('hello world welcome to here!')
+    return HttpResponse('Hello world')
+# current system time
+def mytime(request):
+    now = datetime.now()
+    html = '<html><body>The time now %s</body></html>'% now
+    return HttpResponse(html)
