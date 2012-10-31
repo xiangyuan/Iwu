5b16c2813c6fd04e4174a7479e3d16fa14881a82
xiangyuan <liyajie1209@gmail.com>
2012-10-31 14:12:55 +0800

    update the file and send mail as a test'

--- IWuo/iwuo.py
+++ IWuo/iwuo.py
@@ -4,10 +4,21 @@ import blog
 urls = (
         '/','Home',
         '/blog', blog.blog_app,
+        '/mail','Mail'
         )
 class Home:
     def GET(self):
         return 'Home'
+
+
+class Mail:
+    def GET(self):
+        web.config.smtp_server = 'smtp.gmail.com'
+        web.config.smtp_port = 587
+        web.config.smtp_username = 'liyajie1209@gmail.com'
+        web.config.smtp_password = 'passwor'
+        web.config.smtp_starttls = True
+        web.sendmail('liyajie1209@gmail.com','365283170@qq.com','message','message content')
 
 
 app = web.application(urls,locals())
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 1、使用了python2.7。
-2、django生成初始项目框架。
+2、webpy生成初始项目框架。
 3、本地机器使用了virtalenv进行了环境管理。virtualenv //$ pip install virtualenv  2.virtualenv venv 3. source venv/bin/active 
